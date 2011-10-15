31cce826d91de2b4667673b2c848084a3d76d049
idle sign <idlesign@yandex.ru>
2011-10-15 22:32:39 +0700

    PyPi friendly now.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=False,
     classifiers=[
-    'Development Status :: 4 - Beta',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: BSD License',
